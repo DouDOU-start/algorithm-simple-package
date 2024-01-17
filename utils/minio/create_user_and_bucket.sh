2a6a1afa0c2de7d4981025b238bd2840e3ec8e6c
Allen_huang_4080 <allen.huang@hanglok-tech.cn>
2024-01-17 12:49:13 +0800

    minio创建新用户

--- utils/minio/create_user_and_bucket.sh
+++ utils/minio/create_user_and_bucket.sh
@@ -3,51 +3,77 @@
 # MinIO 服务器信息
 MINIO_ALIAS="myminio"
 MINIO_SERVER_URL="http://10.8.6.30:9000"
-MINIO_ACCESS_KEY="<admin-access-key>"
-MINIO_SECRET_KEY="<admin-secret-key>"
-
-# 新用户信息
-NEW_USER="newusername"
-NEW_PASSWORD="newpassword"
-
-# 新桶名称
-NEW_BUCKET="newbucketname"
+MINIO_ACCESS_KEY="EvuKSqV607cX3C1HC7DV"
+MINIO_SECRET_KEY="5D8KtKcWReCj0QrEo0tVFgstbiLm55D0uLBBcZYK"
 
 # 配置 MinIO 客户端
-mc alias set $MINIO_ALIAS $MINIO_SERVER_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+./mc alias set $MINIO_ALIAS $MINIO_SERVER_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
-# 创建新用户
-mc admin user add $MINIO_ALIAS $NEW_USER $NEW_PASSWORD
+function create_user {
+  local new_user=$1
+  local new_password=$2
 
-# 创建新桶
-mc mb $MINIO_ALIAS/$NEW_BUCKET
+  # 创建新用户
+  ./mc admin user add $MINIO_ALIAS $new_user $new_password
 
-# 创建并应用策略
-POLICY_NAME="${NEW_USER}_policy"
-POLICY_JSON='{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:*"
-      ],
-      "Resource": ["arn:aws:s3:::'$NEW_BUCKET'"]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject",
-        "s3:ListMultipartUploadParts",
-        "s3:AbortMultipartUpload"
-      ],
-      "Resource": ["arn:aws:s3:::'$NEW_BUCKET'/*"]
-    }
-  ]
-}'
-echo $POLICY_JSON | mc admin policy add $MINIO_ALIAS $POLICY_NAME -
-mc admin policy set $MINIO_ALIAS $POLICY_NAME user=$NEW_USER
+  if [ $? -ne 0 ]; then
+    echo "创建用户失败: $new_user"
+    return 1
+  fi
 
-echo "用户 $NEW_USER 和桶 $NEW_BUCKET 已创建并配置策略"
+  # 创建新桶
+  ./mc mb $MINIO_ALIAS/$new_user
+
+  if [ $? -ne 0 ]; then
+    echo "创建桶失败: $new_user"
+    return 1
+  fi
+
+  # 创建并应用策略
+  POLICY_NAME="${new_user}_policy"
+  POLICY_JSON='{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:*"
+        ],
+        "Resource": ["arn:aws:s3:::'$new_user'"]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:*"
+        ],
+        "Resource": ["arn:aws:s3:::'$new_user'/*"]
+      }
+    ]
+  }'
+
+  echo "$POLICY_JSON" > policy.json
+
+  ./mc admin policy create $MINIO_ALIAS $POLICY_NAME policy.json
+
+  rm policy.json
+
+  if [ $? -ne 0 ]; then
+    echo "添加策略失败: $POLICY_NAME"
+    return 1
+  fi
+
+  ./mc admin policy attach $MINIO_ALIAS $POLICY_NAME -u $new_user
+
+  if [ $? -ne 0 ]; then
+    echo "应用策略失败: $POLICY_NAME 到用户 $new_user"
+    return 1
+  fi
+
+  echo "用户 $new_user 和桶 $new_user 已创建并配置策略"
+
+}
+
+# NEW_USER=hantao
+# NEW_PASSWORD=hanglok@8888
+
+# create_user $NEW_USER $NEW_PASSWORD
